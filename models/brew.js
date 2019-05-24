0538375fc1acf1876ce899e36859a7aade1a9a8e
Nick Bueltel <nickbueltel@gmail.com>
2019-05-24 16:38:22 -0500

    update schemas, css, index.ejs

---
+++ models/brew.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var ratingsSchema = new mongoose.Schema({
+    user: String,
+    review: String, 
+    rating: {type:Number, min: 0, max: 5 }
+})
+
+
+var brewSchema = new mongoose.Schema({
+    name: String,
+    ratings: [ratingsSchema],
+
+
+  }, {
+    timestamps: true
+  });
+
+  module.exports = mongoose.model('User', userSchema);
+  module.exports = mongoose.model('Brew', brewSchema);
+  module.exports = mongoose.model('Ratings', ratingsSchema);
+
+
